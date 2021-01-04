22a1947d17712e89e29fc5b00e63547dfe627fe3
arfanlive <60953308+arfanlive@users.noreply.github.com>
2021-01-05 00:22:17 +0600

    added bootstrap

--- auth/register.php
+++ auth/register.php
@@ -1,34 +1,109 @@
-<table width="300" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
-    <tr>
-    <form name="form1" method="post" action="check_register.php">
-    <td>
-    <table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
-    <tr>
-    <td colspan="3"><strong>Member Register </strong></td>
-    </tr>
-    <tr>
-    <td width="78">Full Name</td>
-    <td width="6">:</td>
-    <td width="294"><input name="my_fullname" type="text" id="my_fullname"></td>
-    </tr>
-    <tr>
-    <td width="78">Username</td>
-    <td width="6">:</td>
-    <td width="294"><input name="myusername" type="text" id="myusername"></td>
-    </tr>
-    <tr>
-    <td>Password</td>
-    <td>:</td>
-    <td><input name="mypassword" type="password" id="mypassword"></td>
-    </tr>
-    <tr>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-    <td><input type="submit" name="Submit" value="Register"></td>
-    </tr>
-    </table>
-    </td>
-    </form>
-    </tr>
-</table>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Login</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 
+<link rel="icon" type="image/png" href="https://colorlib.com/etc/lf/Login_v4/images/icons/favicon.ico" />
+
+<link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/lf/Login_v4/vendor/bootstrap/css/bootstrap.min.css">
+
+<link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/lf/Login_v4/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+
+<link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/lf/Login_v4/fonts/iconic/css/material-design-iconic-font.min.css">
+
+<link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/lf/Login_v4/vendor/animate/animate.css">
+
+<link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/lf/Login_v4/vendor/css-hamburgers/hamburgers.min.css">
+
+<link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/lf/Login_v4/vendor/animsition/css/animsition.min.css">
+
+<link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/lf/Login_v4/vendor/select2/select2.min.css">
+
+<link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/lf/Login_v4/vendor/daterangepicker/daterangepicker.css">
+
+<link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/lf/Login_v4/css/util.css">
+<link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/lf/Login_v4/css/main.css">
+
+</head>
+<body>
+<div class="limiter">
+<div class="container-login100" style="background-image: url('https://colorlib.com/etc/lf/Login_v4/images/bg-01.jpg');">
+<div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
+<form class="login100-form validate-form" name="form1" method="post" action="check_register.php">
+<span class="login100-form-title p-b-49">
+Teacher Registration
+</span>
+<div class="wrap-input100 validate-input m-b-23" data-validate="Username is required">
+<span class="label-input100">Fullname</span>
+<input class="input100" type="text" name="my_fullname" id="my_fullname" placeholder="Type your Fullname">
+<span class="focus-input100" data-symbol="&#xf206;"></span>
+</div>
+</span>
+<div class="wrap-input100 validate-input m-b-23" data-validate="Username is required">
+<span class="label-input100">Username</span>
+<input class="input100" type="text" name="myusername" id="myusername" placeholder="Type your username">
+<span class="focus-input100" data-symbol="&#xf206;"></span>
+</div>
+<div class="wrap-input100 validate-input" data-validate="Password is required">
+<span class="label-input100">Password</span>
+<input class="input100" type="password" name="mypassword" id="mypassword" placeholder="Type your password">
+<span class="focus-input100" data-symbol="&#xf190;"></span>
+</div>
+<div class="text-right p-t-8 p-b-31">
+Try your best to always remember your password please
+</div>
+<div class="container-login100-form-btn">
+<div class="wrap-login100-form-btn">
+<div class="login100-form-bgbtn"></div>
+<button class="login100-form-btn">
+Register
+</button>
+</div>
+</div>
+
+
+<div class="flex-col-c p-t-155">
+<span class="txt1 p-b-17">
+Already Registered? Login here then!
+</span>
+<a href="main-login.php" class="txt2">
+Login
+</a>
+</div>
+</form>
+</div>
+</div>
+</div>
+<div id="dropDownSelect1"></div>
+
+<script src="https://colorlib.com/etc/lf/Login_v4/vendor/jquery/jquery-3.2.1.min.js"></script>
+
+<script src="https://colorlib.com/etc/lf/Login_v4/vendor/animsition/js/animsition.min.js"></script>
+
+<script src="https://colorlib.com/etc/lf/Login_v4/vendor/bootstrap/js/popper.js"></script>
+<script src="https://colorlib.com/etc/lf/Login_v4/vendor/bootstrap/js/bootstrap.min.js"></script>
+
+<script src="https://colorlib.com/etc/lf/Login_v4/vendor/select2/select2.min.js"></script>
+
+<script src="https://colorlib.com/etc/lf/Login_v4/vendor/daterangepicker/moment.min.js"></script>
+<script src="https://colorlib.com/etc/lf/Login_v4/vendor/daterangepicker/daterangepicker.js"></script>
+
+<script src="https://colorlib.com/etc/lf/Login_v4/vendor/countdowntime/countdowntime.js"></script>
+
+<script src="https://colorlib.com/etc/lf/Login_v4/js/main.js"></script>
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-23581568-13');
+	</script>
+
+
+
+</body>
+</html>
